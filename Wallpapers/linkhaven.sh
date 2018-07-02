58ee1e473990054232e3496d7c5b3b5581c545aa
Ron Record <rrecord@vmware.com>
2018-07-02 15:26:05 -0700

    Added People, improved usage message, support hard links in linkhaven.sh

--- Wallpapers/linkhaven.sh
+++ Wallpapers/linkhaven.sh
@@ -6,13 +6,26 @@ PHO="$TOP/Photographers"
 PHD="../../Photographers"
 DIG="../../Digital_Desire"
 DRO="../../Photodromm"
+PEO="../../People"
 DES="$DIG"
 SUB="$MOD"
+LN="ln -s"
 ALL=
 TELL=
 
 usage() {
-    echo "Usage: linkhaven [-a] [-n] [-p] [-P] [-d] [-u]"
+    printf "\nUsage: linkhaven [-a] [-h] [-m] [-n] [-p] [-P] [-d] [-u]"
+    printf "\nWhere:"
+    printf "\n\t-a indicates use all combinations of subdirs and destinations"
+    printf "\n\t-h indicates use hard links for duplicates"
+    printf "\n\t\t(default is symbolic links)"
+    printf "\n\t-m indicates use Models subdir and Photographers destination"
+    printf "\n\t-n indicates tell me what you would do but don't do it"
+    printf "\n\t-p indicates use Photographers subdir"
+    printf "\n\t-P indicates use People destination dir"
+    printf "\n\t-d indicates use Photodromm destination dir"
+    printf "\n\t\t(default destination dir is Digital_Desire)"
+    printf "\n\t-u displays this usage message and exits"
     exit 1
 }
 
@@ -35,15 +48,24 @@ linkem() {
                     break
                 done
             fi
+            if [ "$DES" == "$PEO" ]
+            then
+                for wall in $PEO/*/$i
+                do
+                    [ "$wall" == "$PEO/*/$i" ] && continue
+                    SRC=`dirname $wall`
+                    break
+                done
+            fi
             [ -f $SRC/$i ] || {
                 continue
             }
             [ "$TELL" ] && {
-                echo "ln -s $SRC/$i $model"
+                echo "$LN $SRC/$i $model"
                 continue
             }
             rm -f $i
-            ln -s $SRC/$i .
+            $LN $SRC/$i .
         done
         cd ..
     done
@@ -54,17 +76,23 @@ while getopts anmpPdu flag; do
         a)
             ALL=1
             ;;
-        P)
-            DES="$PHD"
-            ;;
         d)
             DES="$DRO"
+            ;;
+        h)
+            LN="ln"
+            ;;
+        m)
+            DES="$PHD"
             ;;
         n)
             TELL=1
             ;;
         p)
             SUB="$PHO"
+            ;;
+        P)
+            DES="$PEO"
             ;;
         u)
             usage
@@ -82,9 +110,13 @@ then
     linkem
     DES="$DRO"
     linkem
+    DES="$PEO"
+    linkem
     SUB="$MOD"
     linkem
     DES="$PHD"
+    linkem
+    DES="$PEO"
     linkem
 
 else
