0f7697286f3d7f17d69585a80405816a88400933
Ron Record <gitlab@ronrecord.com>
2019-04-16 21:07:10 -0700

    Add scripts to display ProfitTrailer logs and set/override Sell Only Mode

---
+++ profittrailer/setPtSOM.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DOMAIN="https://ronrecord.ddns.net"
+PORT=8443
+TOKEN="rrsleig205ujc04tj0x9cugr04slt"
+
+usejq=`type -p jq`
+if [ "$usejq" ]
+then
+    curl -X GET --header 'Accept: application/json' \
+             "${DOMAIN}:${PORT}/api/sellOnlyMode?token=${TOKEN}" \
+             2> /dev/null | jq '.' 
+else
+    curl -X GET --header 'Accept: application/json' \
+             "${DOMAIN}:${PORT}/api/sellOnlyMode?token=${TOKEN}"
+fi
