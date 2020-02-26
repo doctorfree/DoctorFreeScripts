822887c64ac196ab72a2b6ed1dd68934d5bb1667
Ron Record <gitlab@ronrecord.com>
2020-02-25 21:48:57 -0800

    Enable SSH, install and autostart pm2 and MagicMirror

--- MagicMirror/install_magicmirror.sh
+++ MagicMirror/install_magicmirror.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-MM_BASE=$HOME/MagicMirror
+MM_BASE=${HOME}/MagicMirror
 MODULES="MMM-BackgroundSlideshow MMM-DarkSkyForecast MMM-iFrame \
          MMM-ModuleScheduler MMM-NetworkScanner MMM-RAIN-RADAR \
          MMM-Remote-Control MMM-Solar MMM-stocks MMM-SystemStats"
@@ -11,17 +11,17 @@ MODULES="MMM-BackgroundSlideshow MMM-DarkSkyForecast MMM-iFrame \
 }
 
 # Install MMPM
-cd $HOME
+cd ${HOME}
 echo "Installing MMPM and dependencies"
 sudo apt install python3 python3-pip -y && \
       git clone https://github.com/Bee-Mar/mmpm.git && \
       cd mmpm && \
       make && \
-      echo "export PATH=$PATH:$HOME/.local/bin" >> $HOME/.bashrc && \
+      echo "export PATH=$PATH:${HOME}/.local/bin" >> ${HOME}/.bashrc && \
       source ~/.bashrc
 
 # Install MagicMirror
-cd $HOME
+cd ${HOME}
 echo "Installing MagicMirror"
 #mmpm -M
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
@@ -46,8 +46,8 @@ done
 echo "Installing MagicMirror module mmm-hue-lights"
 git clone https://github.com/michael5r/mmm-hue-lights.git
 
-[ -d $HOME/src ] || mkdir $HOME/src
-cd $HOME/src
+[ -d ${HOME}/src ] || mkdir ${HOME}/src
+cd ${HOME}/src
 [ -d Scripts ] || {
     echo "Cloning Scripts repo"
     git clone ssh://gitlab.com/doctorfree/Scripts.git
@@ -69,6 +69,26 @@ sudo apt-get -y install duplicity
 echo "Installing fswebcam utility"
 sudo apt install fswebcam
 
+# For now, use the sample config provided by MagicMirror
+cd ${MM_BASE}/config
+ln -s config.js.sample config.js
+
+# Install pm2
+echo "Installing pm2 utility"
+sudo npm install -g pm2
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 \
+    startup systemd -u pi --hp /home/pi
+
+# Enable and start the SSH service
+sudo systemctl enable ssh
+sudo systemctl start ssh
+
+cd ${HOME}
+printf "#!/bin/bash\ncd ~/MagicMirror\nDISPLAY=:0 npm start\n" > mm.sh
+chmod 755 mm.sh
+pm2 start mm.sh
+pm2 save
+
 echo ""
 echo "==========!! TO DO !!=============="
 echo "Follow instructions at:"
@@ -82,13 +102,13 @@ echo "/usr/local/bin/chktemp"
 echo "/usr/local/bin/gethue"
 echo "/usr/local/bin/getquote"
 echo "/usr/local/bin/send_sms"
-echo "$HOME/MagicMirror/config/config-server.js"
-echo "$HOME/MagicMirror/config/config-iframe.js"
-echo "$HOME/MagicMirror/config/config-weather.js"
-echo "$HOME/MagicMirror/config/config-plex.js"
-echo "$HOME/MagicMirror/config/config-calendar.js"
-echo "$HOME/MagicMirror/config/config-noback.js"
-echo "$HOME/MagicMirror/config/config-default.js"
+echo "${HOME}/MagicMirror/config/config-server.js"
+echo "${HOME}/MagicMirror/config/config-iframe.js"
+echo "${HOME}/MagicMirror/config/config-weather.js"
+echo "${HOME}/MagicMirror/config/config-plex.js"
+echo "${HOME}/MagicMirror/config/config-calendar.js"
+echo "${HOME}/MagicMirror/config/config-noback.js"
+echo "${HOME}/MagicMirror/config/config-default.js"
 echo "==================================="
 echo ""
 echo "Done"
