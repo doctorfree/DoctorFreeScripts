94302c5d7dac85b9a94583b65cfe20d25b37e4d0
Ronald Record <github@ronrecord.com>
2022-07-23 12:28:23 -0700

    Check for required utilities in transwall

--- Wallpapers/transwall.sh
+++ Wallpapers/transwall.sh
@@ -54,6 +54,26 @@ while getopts "crstu" flag; do
 done
 shift $(( OPTIND - 1 ))
 
+have_feh=`type -p feh`
+[ "${have_feh}" ] || {
+  echo "The required 'feh' image display utility is not in the execution PATH"
+  echo "The feh utility can be installed with something like:"
+  printf "\n\tsudo apt install feh\n"
+  echo "Exiting"
+  usage
+}
+
+[ "${pickwall}" ] || [ "${certwall}" ] && {
+  have_dmenu=`type -p dmenu`
+  [ "${have_dmenu}" ] || {
+    echo "The required 'dmenu' utility is not in the execution PATH"
+    echo "The dmenu utility can be installed with something like:"
+    printf "\n\tsudo apt install suckless-tools\n"
+    echo "Exiting"
+    usage
+  }
+}
+
 randomwall=$(ls -1 "${WALLDIR}" | shuf -n 1)
 if [ -f "${NOTES}" ]
 then
@@ -67,12 +87,12 @@ fi
 }
 
 [ "${pickwall}" ] && {
-  certain=$(cd "${WALLDIR}" && ls | dmenu -l 15 -fn "xo4 Terminus:size=9" -i -p "Choose wall:")
+  certain=$(cd "${WALLDIR}" && ls | dmenu -l 15 -fn "xo4 Terminus:size=18" -i -p "Choose wall:")
   cp "${WALLDIR}/$certain" "${WALLPAPER}"
 }
 
 [ "${certwall}" ] && {
-  certain=$(cd "${WALLDIR}" && ls | dmenu -l 15 -fn "xo4 Terminus:size=9" -i -p "Choose wall:")
+  certain=$(cd "${WALLDIR}" && ls | dmenu -l 15 -fn "xo4 Terminus:size=18" -i -p "Choose wall:")
   convwall "${WALLDIR}/$certain"
 }
 
