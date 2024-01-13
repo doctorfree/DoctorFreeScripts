163006455c9b3c69e55c7ffe70ec697d0e99aa0d
Ronald Record <github@ronrecord.com>
2024-01-13 08:29:51 -0800

    Enable tap to click for GDM in tap2click.sh script

--- scripts/tap2click.sh
+++ scripts/tap2click.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 #
 # tap2click - disable or enable the gnome desktop tap to click feature
+#
 
 case "$1" in
+  gdmoff)
+    sudo xhost +SI:localuser:gdm
+    sudo -u gdm dbus-launch \
+      gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click false
+    ;;
+  gdm|gdmon)
+    sudo apt install dbus-x11 -q -y
+    sudo xhost +SI:localuser:gdm
+    sudo -u gdm dbus-launch \
+      gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+    ;;
   off|Off|disable|Disable)
     gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click false
     ;;
@@ -10,6 +22,6 @@ case "$1" in
     gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
     ;;
   *)
-    echo "Usage: tap2click off|on"
+    echo "Usage: tap2click off|on|gdm|gdmoff"
     ;;
 esac
