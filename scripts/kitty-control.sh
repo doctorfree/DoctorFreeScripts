d4af60b26e4b5aa70713f83e06bde01de394b663
Ronald Record <github@ronrecord.com>
2024-02-01 16:32:01 -0800

    Add foreground/background color set to kitty-control and support multiple command loop

--- scripts/kitty-control.sh
+++ scripts/kitty-control.sh
@@ -21,35 +21,51 @@ SOCKET=
 # Kitty remote control options are used to specify which windows/tabs to modify
 OPTS=
 
+brief_usage() {
+  printf "\nUsage: kitty-control [-a] [-b /path/to/image] [-f] [-m <match>] [-t <match>]"
+  printf "\n                     [-s /path/to/socket] [-u | -h] [back <color>] [fore <color>]"
+  printf "\n                     [dark | font [num] | list | load [subdir] | tran [opacity]]"
+  [ "$1" == "noexit" ] || {
+    printf "\n\nFor a full usage message run 'kitty-control -u'\n\n"
+    exit 1
+  }
+}
+
 usage() {
-  printf "\nUsage: kitty-control [ dark | font [num] | list | load [subdir] | tran [opacity]]"
-  printf "\n                     [-a] [-f] [-m <match>] [-t <match>] [ -h | -u ] [-s /path/to/socket]"
+  brief_usage noexit
   printf "\nWhere:"
+  printf "\n    'back color' Sets the background color to 'color'"
+  printf "\n           If 'color' is 'reset' restores foreground and background to startup value"
   printf "\n    'dark' Sets the Kitty background opacity to 1.0 (fully opaque)"
   printf "\n           Can use 'dark' or 'opaque'"
-  printf "\n    'font' Sets the font size to 'num'"
+  printf "\n    'font num' Sets the font pointsize to 'num'"
   printf "\n           Can use 'font', 'fontsize', 'fontminus', or 'fontplus'"
   printf "\n           The second argument specifies the font size, either absolute, +, or -"
-  printf "\n           e.g. 'kitty-control font 24' would set the font size to 24 points"
+  printf "\n           e.g. 'kitty-control fontsize 24' would set the font size to 24 points"
   printf "\n                'kitty-control font +2' would increase the font size by 2 points"
   printf "\n                'kitty-control font' without argument resets the font size to default"
+  printf "\n    'fore color' Sets the foreground color to 'color'"
+  printf "\n           If 'color' is 'reset' restores foreground and background to startup value"
   printf "\n    'list' Displays information on Kitty windows"
-  printf "\n           Can be used in conjunction with '-m <match>' and '-t <match> to limit the output"
-  printf "\n    'load' Reloads the Kitty configuration in ~/.config/kitty/kitty.conf"
-  printf "\n           Can use 'load', 'reset', or 'reload'"
+  printf "\n    'load [subdir]' Reloads the Kitty configuration in ~/.config/kitty/kitty.conf"
+  printf "\n           Can use 'load' or 'reload'"
   printf "\n           Specify a second argument to load ~/.config/kitty/<subdir>/kitty.conf"
   printf "\n           e.g. 'kitty-control load tv' would load ~/.config/kitty/tv/kitty.conf"
   printf "\n           'kitty-control load default' restores the kitty.conf used to start this instance"
-  printf "\n    'tran' Sets the Kitty background opacity to 0.8"
-  printf "\n           Can use 'tran', 'trans' or 'transparent'"
+  printf "\n    'tran [opacity]' Sets the Kitty background opacity to 0.8"
+  printf "\n           Can use 'tran', 'opacity', 'trans' or 'transparent'"
   printf "\n           Specify a second argument to set a custom background opacity:"
   printf "\n           e.g. 'kitty-control transparent 0.9'"
   printf "\n    '-a' Indicates modify all windows rather than just the currently active OS window"
+  printf "\n    '-b /path/to/image' sets the background image for the specified Kitty windows"
+  printf "\n        If /path/to/image is 'none' then any existing image will be removed"
   printf "\n    '-f' Indicates toggle fullscreen"
   printf "\n    '-m <match>' Specifies the window to match"
   printf "\n    '-t <match>' Specifies the tab to match"
+  printf "\n        Window/Tab matching can be used in conjunction with most kitty-control commands"
   printf "\n        If <match> is 'help' the Kitty documentation URL for matching will be displayed"
   printf "\n    '-s /path/to/socket' Specifies the socket Kitty is listening on if enabled"
+  printf "\n        If /path/to/socket is 'help' some help on configuring a Kitty socket is provided"
   printf "\n    '-u or -h' Displays this usage message and exits"
   printf "\nAdjusting the background opacity or font size requires the original kitty.conf"
   printf "\nthat was used for this instance of Kitty to have enabled the following:"
@@ -107,19 +123,47 @@ kitty-reload() {
   kill -SIGUSR1 $(pidof kitty)
 }
 
-kitty-opaque() {
-  kitty @ ${SOCKET} set-background-opacity ${OPTS} 1.0
+kitty-background() {
+  kitty @ ${SOCKET} set-background-image ${OPTS} $1
 }
 
-kitty-transparent() {
+kitty-opacity() {
   kitty @ ${SOCKET} set-background-opacity ${OPTS} $1
 }
 
-[ "$1" ] || usage
-while getopts ":afm:s:t:hu" flag; do
+set-background() {
+  if [ "$1" == "reset" ]; then
+    kitty @ ${SOCKET} set-colors ${OPTS} --reset
+  else
+    kitty @ ${SOCKET} set-colors ${OPTS} background=$1
+  fi
+}
+
+set-foreground() {
+  if [ "$1" == "reset" ]; then
+    kitty @ ${SOCKET} set-colors ${OPTS} --reset
+  else
+    kitty @ ${SOCKET} set-colors ${OPTS} foreground=$1
+  fi
+}
+
+[ "$1" ] || brief_usage
+while getopts ":ab:fm:s:t:hu" flag; do
   case $flag in
     a)
       OPTS="$OPTS -a"
+      ;;
+    b)
+      if [ "${OPTARG}" == "none" ]; then
+        kitty-background "${OPTARG}"
+      else
+        if [ -f "${OPTARG}" ]; then
+          kitty-background "${OPTARG}"
+        else
+          printf "\nSpecified Kitty background image ${OPTARG} not found\n"
+          exit 1
+        fi
+      fi
       ;;
     f)
       toggle-fullscreen
@@ -133,7 +177,24 @@ while getopts ":afm:s:t:hu" flag; do
       fi
       ;;
     s)
-      SOCKET="--to unix:$OPTARG"
+      if [ "${OPTARG}" == "help" ]; then
+        printf "\nStart kitty as:"
+        printf "\n\tkitty -o allow_remote_control=yes --listen-on unix:/tmp/mykitty"
+        printf "\nThe kitty '--listen-on' option tells kitty to listen for control messages"
+        printf "\nat the specified UNIX-domain socket. See kitty --help for details.\n"
+        printf "\nNow you can control this instance of kitty using the"
+        printf "\n\tkitten @ --to command line argument to kitten @. For example:"
+        printf "\n\tkitten @ --to unix:/tmp/mykitty ls"
+        printf "\nRemote control via a socket can be enabled in kitty.conf by setting:"
+        printf "\n\tallow_remote_control yes"
+        printf "\n\tlisten_on unix:/tmp/mykitty\n"
+        printf "\nTo use this with kitty-control set SOCKET=\"--to unix:/tmp/mykitty\""
+        printf "\nin kitty-control or invoke kitty-control with '-s /tmp/mykitty'."
+        printf "\nFor example: kitty-control -s /tmp/mykitty fontsize 24\n\n"
+        exit 0
+      else
+        SOCKET="--to unix:$OPTARG"
+      fi
       ;;
     t)
       if [ "${OPTARG}" == "help" ]; then
@@ -154,55 +215,80 @@ while getopts ":afm:s:t:hu" flag; do
 done
 shift $(( OPTIND - 1 ))
 
-case $1 in
-  list|ls)
-      kitty-list
+while [ "$1" ]
+do
+  case $1 in
+    back*)
+      set-background "$2"
+      shift 2
       ;;
-  load|reload|reset)
+    fore*)
+      set-foreground "$2"
+      shift 2
+      ;;
+    list|ls)
+      kitty-list
+      shift
+      ;;
+    load|reload)
       if [ "$2" ]; then
         if [ -f "${CONFDIR}/$2/kitty.conf" ]; then
           kitty-reload "$2"
+          shift 2
         else
           [ "$2" == "default" ] || {
             printf "\n ${CONFDIR}/$2/kitty.conf does not exist\n"
+            shift
           }
         fi
       else
         kitty-reload
+        shift
       fi
       ;;
-  dark|opaque)
-      kitty-opaque
+    dark|opaque)
+      kitty-opacity "1.0"
+      shift
       ;;
-  fontmin*|fontdec*)
+    fontmin*|fontdec*)
       if [ "$2" ]; then
         kitty-fontminus "$2"
+        shift 2
       else
-        printf "\nFont size decrement must be specified with the 'fontminus' argument"
-        usage
+        kitty-fontminus 1
+        shift
       fi
       ;;
-  fontplus|fontinc*)
+    fontplus|fontinc*)
       if [ "$2" ]; then
         kitty-fontplus "$2"
+        shift 2
       else
-        printf "\nFont size increment must be specified with the 'fontplus' argument"
-        usage
+        kitty-fontplus 1
+        shift
       fi
       ;;
-  font*)
+    font*)
       if [ "$2" ]; then
         kitty-fontsize "$2"
+        shift 2
       else
         kitty-fontsize 0
+        shift
       fi
       ;;
-  tran*)
-      [ "$2" ] && OPACITY="$2"
-      kitty-transparent "${OPACITY}"
+    opacity|tran*)
+      if [ "$2" ]; then
+        OPACITY="$2"
+        shift 2
+      else
+        shift
+      fi
+      kitty-opacity "${OPACITY}"
       ;;
     *)
       printf "\nUnsupported Kitty control command: $1"
       usage
       ;;
-esac
+  esac
+done
